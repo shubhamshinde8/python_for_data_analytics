32f0e4b443b89c0b90d4950b26f05bac61b1a9ab
shubhamshinde8 <shubhamshinde8746@gmail.com>
2026-03-02 09:19:55 -0800

    understanding if else statement and loops

---
+++ if_else.py
@@ -0,0 +1,25 @@
+
+# understaing decision making statements and loops 
+
+num=int(input("Enter a Number : "))
+
+if num>0 : 
+       print("Number is positive ")
+elif num==0:
+       print("Number is Zero ")
+else :
+       print("Number is negetive ")
+
+
+# check user is eligible for vote or not 
+
+
+age=int(input("Enter Your age : "))
+
+if age>=18 :
+       print(" You are elegible for vote ")
+else:
+       print("You are not eligible for voting ")
+
+
+# 
