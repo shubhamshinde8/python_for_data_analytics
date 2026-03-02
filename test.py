afb3975e9e443374f03179e670615b01c75a73f8
shubhamshinde8 <shubhamshinde8746@gmail.com>
2026-03-02 08:57:53 -0800

    understanding python variables, keywords, functions

--- test.py
+++ test.py
@@ -1,1 +1,65 @@
 
+# variables in python 
+
+
+# name1='shubham shinde'
+
+# print(name1)
+
+# a=1
+# b=2
+# sum=a+b
+# print(sum)
+
+
+# score=88.09
+
+# print(score)
+
+
+# colors=['red','blue','pink','yellow','brown','silver','orange']
+
+# print(colors[0])
+
+
+# # check maximum number from given list
+
+# list1=[67,43,12,56,90,7,5,4]
+
+# max=list1[0]
+
+
+
+# def find_maximum(numbers):
+#     # Handle the case where the list might be empty
+#     if not numbers:
+#         return None
+
+#     # Start by assuming the first number is the largest
+#     max_val = numbers[0]
+
+#     # Loop through the list starting from the second element
+#     for num in numbers:
+#         if num > max_val:
+#             # If we find a bigger number, update max_val
+#             max_val = num
+            
+#     return max_val
+
+# # Example usage:
+# my_list = [14, 52, 5, 98, 31, 7]
+# result = find_maximum(my_list)
+
+# print(f"The maximum number in the list is: {result}")
+
+
+
+
+
+def printHello(name):
+    print("Your Name is : "+name)
+    # return 0
+
+a=input("Enter Your Name : ")
+printHello(a)
+
